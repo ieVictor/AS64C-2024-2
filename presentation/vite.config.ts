a55d1aad218ba0685a8c90a73d79d8b362b8b57d
Victor Gabriel <victor_tiss@outlook.com>
2024-12-15 19:20:03 -0300

    test: add routes & change base

--- presentation/vite.config.ts
+++ presentation/vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     rollupOptions: {
