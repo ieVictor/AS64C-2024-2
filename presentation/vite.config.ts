e178b07522ac2b1d2a64cf5350b7d824805a2e54
Victor Gabriel <victor_tiss@outlook.com>
2024-12-15 19:09:20 -0300

    build(vite): add alias config

--- presentation/vite.config.ts
+++ presentation/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -6,5 +7,13 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
     outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
   },
 });
